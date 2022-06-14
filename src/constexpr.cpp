761575ba2949dd81efa36ebeb41db95276e53929
Asahi Takenouchi <asahi.taken@gmail.com>
2022-06-15 00:43:35 +0900

    feat: create constexpr.cpp

---
+++ src/constexpr.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+namespace {
+// constexpr std::string s = "Hello, World!";
+constexpr std::string_view s_view = "Hello world";
+// constexpr std::vector<int> v = {1, 2, 3, 4, 5};
+}  // namespace
+
+int main() { return 0; }
